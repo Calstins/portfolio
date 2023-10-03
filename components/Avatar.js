cd73e5dcb0f8f7bd7ac1dafe112f6600fdfec003
Calstins <opulecalebtins@gmail.com>
2023-10-03 07:43:23 -0700

    add tsparticle library to the project

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,12 @@
+//next image
+import Image from 'next/image'
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className='hidden xl:flex xl:max-w-none'>
+      <Image src={'/caleb.png'} width={191} height ={255} className='translate-z-0 w-full h-full' alt='opule-caleb'/>
+    </div>
+  );
 };
 
 export default Avatar;
