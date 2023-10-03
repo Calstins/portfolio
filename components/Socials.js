95a535da7a28e10ecdb0d1d33c73ec9deb13dc1d
Calstins <opulecalebtins@gmail.com>
2023-10-03 09:38:11 -0700

    created the header component

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import {RiGithubLine, RiInstagramLine, RiLinkedinLine} from 'react-icons/ri'
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className="flex items-center gap-x-5 text-lg">
+    <Link href={'https://github.com/Calstins'} className="hover:text-accent transition-all duration-300"
+    >
+      <RiGithubLine/>
+    </Link>
+    <Link href={'https://www.instagram.com/calstins/'} className="hover:text-accent transition-all duration-300"
+    >
+      <RiLinkedinLine/>
+    </Link>
+    <Link href={'https://www.instagram.com/calstins/'} className="hover:text-accent transition-all duration-300"
+    >
+      <RiInstagramLine/>
+    </Link>
+  </div>;
 };
 
 export default Socials;
