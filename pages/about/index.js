ca0b5354fdbd88f8e885653900c430968e40785e
Calstins <opulecalebtins@gmail.com>
2023-10-07 18:42:14 -0700

    fix error vercel deployment on key indexing

--- pages/about/index.js
+++ pages/about/index.js
@@ -107,8 +107,8 @@ import { motion } from 'framer-motion';
 import {fadeIn} from '../../variants'
 
 const About = () => {
-  const [index, setIndex] = useState(0)
-  console.log(index);
+  const [indexes, setIndexes] = useState(0)
+  console.log(indexes);
 
   return <div className=' bg-primary/30 py-32 text-center xl:text-left'>
     {/* circles */}
@@ -188,15 +188,15 @@ const About = () => {
         <div 
           className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'
         >
-          {aboutData.map((item, items)=>{
+          {aboutData.map((item, index)=>{
             return (
               <div 
-                key = {items} 
+                key = {index} 
                 className = {`${
-                  index === items && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                  indexes === index && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
                 } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
                 onClick={()=>
-                  setIndex(items)
+                  setIndexes(index)
                 }
               >
                 {item.title}
@@ -205,9 +205,9 @@ const About = () => {
           })}
         </div>
         <div className= 'py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
-          {aboutData[index].info.map((item, items)=>{
+          {aboutData[indexes].info.map((item, index)=>{
             return(
-              <div key = {items} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'>
+              <div key = {index} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'>
                 <div className='font-light mb-2 md:mb-0'>{item.title}</div>
                 <div className="hidden md:flex ">-</div>
                 <div>{item.stage}</div>
