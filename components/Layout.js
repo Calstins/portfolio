37d51ba780a4daa158b0a9456212c1031c83b01d
Calstins <opulecalebtins@gmail.com>
2025-05-06 18:32:49 +0100

    fix mobil responsive issue2

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,27 @@
-import {Sora} from "next/font/google"
+import { Sora } from 'next/font/google';
 
 //font settings
 const sora = Sora({
-  subsets:['latin'],
+  subsets: ['latin'],
   variable: '--font-sora',
-  weight:['100','200','300','400','500','600','700','800']
-})
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
 //components
-import Nav from '../components/Nav'
-import Header from '../components/Header'
-import TopLeftImg from '../components/TopLeftImg'
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+import TopLeftImg from '../components/TopLeftImg';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-    <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
-      <TopLeftImg/>
-      <Nav/>
-      <Header/>
-      {children}
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+    >
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      <main className="flex-1 overflow-y-auto sm:overflow-hidden">
+        {children}
+      </main>
     </div>
   );
 };
